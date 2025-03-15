035aa802638516707bad32ac319a4b76004b2ff4
leoo955 <164084139+leoo955@users.noreply.github.com>
2025-03-15 16:38:14 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,16 @@
-from flask import Flask 
+from flask import Flask
 from threading import Thread
+import os  # <-- Ajout pour récupérer le port
 
-app= Flask('')
+app = Flask('')
 
 @app.route('/')
 def home():
     return "Je suis en Ligne"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.getenv("PORT", 8080))  # <-- Utilise le port défini par Render
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
